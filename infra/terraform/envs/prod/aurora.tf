9b25ddb42a29ce472a73a5f87dabe9b4bde3aa23
MarcelleTabosa <marcelle.tabosa.97@gmail.com>
2025-10-05 22:03:18 -0300

    fix: deployment 3

--- infra/terraform/envs/prod/aurora.tf
+++ infra/terraform/envs/prod/aurora.tf
@@ -1,3 +1,14 @@
+resource "aws_db_subnet_group" "aurora" {
+  name       = "${var.project_name}-aurora"
+  subnet_ids = module.vpc.private_subnets
+
+  tags = {
+    Name        = "${var.project_name}-aurora"
+    Environment = "prod"
+    Project     = var.project_name
+  }
+}
+
 module "aurora" {
   source  = "terraform-aws-modules/rds-aurora/aws"
   version = "9.2.0"
@@ -22,7 +33,7 @@ module "aurora" {
   }
 
   vpc_id                 = module.vpc.vpc_id
-  subnets                = module.vpc.private_subnets
+  db_subnet_group_name   = aws_db_subnet_group.aurora.name 
   vpc_security_group_ids = [aws_security_group.aurora.id]
 
   skip_final_snapshot = true
@@ -32,6 +43,8 @@ module "aurora" {
     Environment = "prod"
     Project     = var.project_name
   }
+
+  depends_on = [aws_db_subnet_group.aurora]
 }
 
 resource "aws_security_group" "aurora" {
