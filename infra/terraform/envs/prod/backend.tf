de4a2ae704b7260263bd06c441fe44674b5b16c1
MarcelleTabosa <marcelle.tabosa.97@gmail.com>
2025-10-06 22:46:44 -0300

    fix: region

--- infra/terraform/envs/prod/backend.tf
+++ infra/terraform/envs/prod/backend.tf
@@ -2,7 +2,7 @@ terraform {
   backend "s3" {
     bucket  = "manafood-bucket-terraform-tfstate"
     key     = "mana-food-prod.tfstate"
-    region  = "us-east-1"
+    region  = "sa-east-1"
     encrypt = true
   }
 }
