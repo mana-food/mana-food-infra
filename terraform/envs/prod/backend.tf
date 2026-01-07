07522856fab6e2d1bda9638095383149edf599a1
MarcelleTabosa <marcelle.tabosa.97@gmail.com>
2026-01-07 07:52:43 -0300

    fix: nome  bucket

--- terraform/envs/prod/backend.tf
+++ terraform/envs/prod/backend.tf
@@ -1,8 +1,9 @@
 terraform {
   backend "s3" {
-    bucket  = "manafood"
-    key     = "mana-food-prod.tfstate"
-    region  = "us-east-1"
-    encrypt = true
+    bucket         = "fiap-tf-state-mana-food"
+    key            = "prod/terraform.tfstate"
+    region         = "us-east-1"
+    encrypt        = true
+    dynamodb_table = "terraform-state-lock"
   }
 }
