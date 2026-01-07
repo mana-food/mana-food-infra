ef2deb9f2eeca04b4acec82ac7f873fd1c9d04da
MarcelleTabosa <marcelle.tabosa.97@gmail.com>
2026-01-07 07:57:58 -0300

    fix: dynamondb

--- terraform/envs/prod/backend.tf
+++ terraform/envs/prod/backend.tf
@@ -4,6 +4,6 @@ terraform {
     key            = "prod/terraform.tfstate"
     region         = "us-east-1"
     encrypt        = true
-    dynamodb_table = "terraform-state-lock"
+    # dynamodb_table = "terraform-state-lock"
   }
 }
